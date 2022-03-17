484ed621899dbd5c74b931f5685eb455cb7ce15a
root <oluwafemikayodej@gmail.comgit config --global user.name Jayodegit config --global user.email oluwafemikayodej@gmail.com>
2022-03-17 03:32:31 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,8 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; i++)									{
+		for (i = 0; i <= n; i++)
+		{
 			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
@@ -39,7 +40,8 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');						_putchar((k % 10) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
