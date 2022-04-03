b033403415fd14fe51071806e6c15ceec0b13642
root <oluwafemikayodej@gmail.comgit config --global user.name Jayodegit config --global user.email oluwafemikayodej@gmail.com>
2022-04-03 15:53:54 -0700

    create_static_lib.sh

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_FUNC
-#define MAIN_FUNC
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -24,4 +24,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_FUNC */
+#endif /* MAIN_H */
