d3ea1aa802453a3c91e63e746beeee40fe520c52
Jayode <97662506+Jayode@users.noreply.github.com>
2022-04-03 18:27:14 +0100

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,8 @@
+#ifndef MAIN_FUNC
+#define MAIN_FUNC
+
 #include <stdio.h>
-#ifndef _MAIN_H
-#define _MAIN_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,4 +23,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+
+#endif /* MAIN_FUNC */
